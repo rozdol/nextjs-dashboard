1a7d154b2028b46b957a36cea91bee9223984112
Alex Titoff <rozdol@gmail.com>
2024-06-09 15:16:00 +0300

    Full

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Main',
+};
 
 export default function Page() {
   return (
